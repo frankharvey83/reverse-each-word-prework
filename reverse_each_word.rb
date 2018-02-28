790d3c8f4328c640d7aaf61ee3611ed31129cc70
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 19:38:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   arr = string.split(" ")
   new_arr = []
-  arr.each do {|word| word.reverse }
-    arr
+  arr.collect { |w| new_arr.push(w.to_s.reverse) }
+  new_arr.join(" ")
 end
